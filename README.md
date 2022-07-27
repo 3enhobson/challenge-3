a545bbd44ce098a108eada7b2ca2c800f1eb3cf8
Ben Hobson <bhobson818@gmail.com>
2022-07-27 11:16:10 -0700

    finished

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Password Generator Starter Code
+
+## Description
+
+For this project I had to use my knowledge of Javascript to create a random password generator. This project came with starter code that I bult upon to finish the assignment. I created variables and arrays containing the different characters to use as options for the potential passwords that were to be generated. I used prompts and alerts to comunicate and get information from the user and I took the information I recieved and used if and for statements to randomly generate the passwords. This project really pushed me to my limits but proved to myself that with time and dedication I am capable of learning new techniques that are very useful.  
+
+## Usage
+
+![screenshot](assets/password%20generator.jpeg)
+    
--- script.js
+++ script.js
@@ -4,45 +4,83 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function randomInt(min, max) {
+  if (!max) {
+    min = max
+    max = 0
+  }
+  var rand = Math.random()
+  return Math.floor(min * (1 - rand) + rand * max)
+}
+
+function getRandomItem(list) {
+  return list[randomInt(0, list.length)]
+  
+}
+
 var passwordArray = []
 
 function generatePassword() {
 
-var userAnswer = window.prompt("How many characters would you like in your password?")
+  var userAnswer = window.prompt("How many characters would you like in your password?")
 
-var passwordLength = parseInt(userAnswer)
+  var passwordLength = parseInt(userAnswer)
 
-if (isNaN(passwordLength)){
-  window.prompt("Must be a number!")
-  return
+  if (isNaN(passwordLength)) {
+    window.alert("Must be a number!")
+    return
+  }
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.prompt("Password must be between 8 & 128 characters.")
+    return
+  }
+
+  var includeLowercase = window.confirm("Would you like to include lowercase letter?")
+  var includeUppercase = window.confirm("Would you like to include uppercase letters?")
+  var includeNumbers = window.confirm("Would you like to include numbers?")
+  var includeSymbols = window.confirm("Would you like to include symbols?")
+
+  var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var symbolList = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "{", "}", "[", "]", "|", ":", ";", "?", "<", ">", ",", "."]
+
+
+
+  if (includeLowercase === true) {
+    passwordArray.push(lowercaseList)
+  }
+  if (includeUppercase === true) {
+    passwordArray.push(uppercaseList)
+  }
+  if (includeNumbers === true) {
+    passwordArray.push(numberList)
+  }
+  if (includeSymbols === true) {
+    passwordArray.push(symbolList)
+  }
+
+  if (passwordArray.length === 0) {
+    window.alert("You must select at least one catagory!")
+    return
+  }
+
+  var generatedPassword = ""
+
+  for (var i = 0; i < passwordLength; i++) {
+    //randomList picks a list 
+    var randomList = getRandomItem(passwordArray)
+    //randomCharacter picks a character from whatever list was picked
+    var randomCharacter = getRandomItem(randomList)
+
+    generatedPassword += randomCharacter
+  }
+
+  return generatedPassword
 }
 
-if (passwordLength < 8 || passwordLength > 128){
-  window.prompt("Password must be between 8 & 128 characters.")
-} else {
- var includeLowercase = window.confirm("Would you like to include lowercase letter?")
- var includeUppercase = window.confirm("Would you like to include uppercase letters?")
- var includeNumbers = window.confirm("Would you like to include numbers?")
- var includeSymbols = window.confirm("Would you like to include symbols?")
 
- if (includeLowercase){
-  passwordArray.push("a", "b","c", "d", "e", "f","g", "h", "i", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
- }
- if (includeUppercase){
-  passwordArray.push("A", "B","C", "D", "E", "F","G", "H", "I", "J", "K", "L", "M", "N", "O", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
- }
- if (includeNumbers){
-  passwordArray.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
- }
-if (includeSymbols){
-  passwordArray.push("~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "{", "}", "[", "]", "|", ":", ";", "?", "<", ">", ",", ".")
-}
-var finalArray = []
-}
-
-
-
-}
 
 
 // Write password to the #password input
